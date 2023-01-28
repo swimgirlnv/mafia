c5868df37acea5b9e47b8e9a5cfbabb74b582b2f
Theo Fernandez <theofernandez@theos-mbp.devices.brown.edu>
2023-01-28 17:10:44 -0500

    Created Node.js backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./Home";
 import Lobby from "./Lobby";
+
+
 
 function App() {
   return (
